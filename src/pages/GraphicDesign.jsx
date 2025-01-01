23a5d064c837eb35cf2867b1ada3f2c78edf9273
Your New Username <elijahkel22@gmail.com>
2025-01-01 13:37:12 +0100

    I commit

--- src/pages/GraphicDesign.jsx
+++ src/pages/GraphicDesign.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const GraphicDesignPage = () => {
   return (
-    <div className="bg-gray-50 p-6 md:p-12">
-      <div className="max-w-6xl mx-auto bg-white shadow-lg rounded-lg p-8">
+    <div className="bg-gray-50 p-6 md:p-8">
+      <div className=" bg-gray-50 rounded-lg p-8">
         <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-6 text-center">
           Graphic Design and Branding Services
         </h1>
